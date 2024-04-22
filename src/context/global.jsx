dfbd538c0f8356865c0a95c5e415bd06709e6a7d
JuanMorello506 <morellojuan992@gmail.com>
2024-04-22 19:19:19 -0300

    Setting actions and reducer function in the global context

--- src/context/global.jsx
+++ src/context/global.jsx
@@ -1,11 +1,24 @@
-import {createContext, useContext} from "react";
+import {createContext, useContext, useReducer, useEffect} from "react";
+
 
 const GlobalContext = createContext();
 
 const baseUrl = "https:api.jikan.moe/v4"
 
+//actions
+const LOAD = `LOADING`
+const SEARCH = `SEARCH`
+const GET_POPULAR_ANIME = `GET_POPULAR_ANIME`
+const GET_UPCOMING_ANIME = `GET_UPCOMING_ANIME`
+
+//reducer
+const reducer = (state, action) => {
+    return state;
+}
+
 export const GlobalContextProvider = ({children}) => {
-    const InitialState = {
+    //intial state
+    const initialState = {
         popularAnime:[],
         upcomingAnime:[],
         airingAnime:[],
@@ -14,8 +27,27 @@ export const GlobalContextProvider = ({children}) => {
         searchResults:[],
         loading:false
     }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    //fetching popular animes
+    const getPopularAnime = async () => {
+        const response = await fetch(`${baseUrl}/top/anime`);
+        const data = await response.json();
+        console.log(data);
+
+    }
+
+    //initial render
+    useEffect(() => {
+        getPopularAnime();
+    }, [])
+
     return(
-        <GlobalContext.Provider value={'Hello'}>
+        
+        <GlobalContext.Provider value={{
+            ...state
+        }}>
             {children}
         </GlobalContext.Provider>
     )
